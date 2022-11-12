eaffc7d7b9f13477cbbe3f66dce2ac56cbfd2f9a
Vincent Panis <v.panis@hotmail.fr>
2022-11-12 12:52:49 +0100

    Addition of function read_bulk in Bulk class

--- bulk/bulk.py
+++ bulk/bulk.py
@@ -7,19 +7,29 @@ Main Bulk class.  Defines:
   - Junction
 """
 
-import os
 from pyNastran.bdf.bdf import BDF
 
 
 class Bulk(BDF):
     """
-
+    NASTRAN BDF Reader/Writer/Editor class with additional data:
+        - 2D part list
+        - Junction list
     """
-    pass
+    def __init__(self):
+        """
+        Initialize the Bulk object
+        """
+        # Initialize BDF class
+        super().__init__()
 
+        # store 2D parts
+        self.part_2d = {}
+        # store fasteners
+        self.fasteners = {}
+        # store junctions
+        self.junctions = {}
 
-if __name__ == '__main__':
-    test_path = os.path.join('..', 'models')
-    bdf_filename = os.path.join(test_path, 'FEM-001.bdf')
-    model = BDF()
-    model.read_bdf(bdf_filename)
+    def read_bulk(self, bulk_filename):
+        self.read_bdf(bulk_filename)
+        # set self_2d with function _get_part_2d
