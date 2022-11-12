b0729c20067af0c6af508a8492ffe48127734dcb
Vincent Panis <v.panis@hotmail.fr>
2022-11-12 18:12:24 +0100

    Addition of function test_bulk in bulk_test.py

--- bulk/test/bulk_test.py
+++ bulk/test/bulk_test.py
@@ -4,15 +4,26 @@
 import os
 
 
-def main():
-    """the interface for bulk_test"""
+def test_read_bulk(model_filename):
+    """read_bulk test """
     from bulk.bulk import Bulk
 
+    # create instance of class Bulk
+    bulk = Bulk()
+    # read testing model
+    bulk.read_bulk(model_filename)
+    # print model stat
+    print(bulk.get_bdf_stats())
+
+
+def main():
+    """the interface for bulk_test"""
+    # set file name of testing model
     test_path = os.path.join('..', '..', 'models')
     bdf_filename = os.path.join(test_path, 'FEM-001.bdf')
 
-    bulk = Bulk()
-    bulk.read_bulk(bdf_filename)
+    # read testing model
+    test_read_bulk(bdf_filename)
 
 
 if __name__ == '__main__':
