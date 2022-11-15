d464d9ab85be55e0231a97a4c01b1bc5919c4f07
matthias-hbt <matthias31770@gmail.com>
2022-11-15 18:59:28 +0100

    comments added in bulk_utils.py

--- bulk/bulk_utils.py
+++ bulk/bulk_utils.py
@@ -2,7 +2,8 @@ def _set_selected_nids(bulk):
     """
     Select node ids from which search for attached element ids
     """
-    if bulk.selected_nids.len() == 0:  # if not bulk.selected_nids:
+    #if selected_nids is empty
+    if not bulk.selected_nids:
         bulk.selected_nids = bulk.bulk_nids[0]
     else
         bulk.selected_nids = [bulk.attached_eids[i].node_ids for i in bulk.attached_eids]
@@ -13,14 +14,14 @@ def _get_attached_eid_from_nid(bulk):
     """
     len = bulk.attached_eids.len()
     temp = get_eids_from_nid(bulk.selected_nids)
-
     for i in temp:
         if temp[i] in bulk.attached_eids:
             continue
         else
+            #put the element in attached_eids if not already in
             bulk.attached_eids.append(temp[i])
-    # On vérifie si des éléments ont été ajoutés à attached_eids
 
+    #Verify if elements has been added in attached_eids list
     if len == bulk.attached_eids.len():
         cur_part = PART()
         cur_part.elements = bulk.attached_eids
