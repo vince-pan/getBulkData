417b17e241e0545681cabdb6d235db2bfe29a215
matthias-hbt <matthias31770@gmail.com>
2022-11-15 23:02:04 +0100

    add underscore in front of intern variable, _remove_selected_nids_from_bulk_nids fonction creation

--- bulk/bulk_utils.py
+++ bulk/bulk_utils.py
@@ -1,30 +1,38 @@
+from pyNastran.bdf.bdf import _get
 def _set_selected_nids(bulk):
     """
     Select node ids from which search for attached element ids
     """
     #if selected_nids is empty
-    if not bulk.selected_nids:
-        bulk.selected_nids = bulk.bulk_nids[0]
+    if not bulk._selected_nids:
+        bulk._selected_nids.append(bulk._bulk_nids[0])
     else:
-        bulk.selected_nids = [bulk.attached_eids[i].node_ids for i in bulk.attached_eids]
+        [bulk._selected_nids.append(bulk._attached_eids[i].node_ids) for i in bulk._attached_eids]
 
 def _get_attached_eid_from_nid(bulk):
     """
     Get attached element ids from a node ids list
     """
-    len = bulk.attached_eids.len()
-    temp = get_eids_from_nid(bulk.selected_nids)
-    for i in temp:
-        if temp[i] in bulk.attached_eids:
+    length = len(bulk._attached_eids)
+    temp = get_eids_from_nid(bulk._selected_nids)
+    for i in range (len(temp)):
+        if temp[i] in bulk._attached_eids:
             continue
         else:
             #put the element in attached_eids if not already in
-            bulk.attached_eids.append(temp[i])
+            bulk._attached_eids.append(temp[i])
 
     #Verify if elements has been added in attached_eids list
-    if len == bulk.attached_eids.len():
+    if length == bulk._attached_eids.len():
         cur_part = PART()
-        cur_part.elements = bulk.attached_eids
-        bulk.part_list.append(cur_part)
+        cur_part.elements = bulk._attached_eids
+        bulk._part_list.append(cur_part)
     else:
         pass
+
+def _remove_selected_nids_from_bulk_nids(bulk):
+    """
+    remove nodes in _selected_nids from _bulk_ids
+    """
+    for i in range (len(bulk._selected_nids)):
+        bulk._bulk_nids.remove(bulk._selected_nids[i])
