94243d29ff5daa45de3285f107dd6ec00471b64e
matthias-hbt <matthias31770@gmail.com>
2022-11-15 19:41:37 +0100

    test_get_parts_2d fonction definition

--- bulk/test/bulk_test.py
+++ bulk/test/bulk_test.py
@@ -15,6 +15,14 @@ def test_read_bulk(model_filename):
     # print model stat
     print(bulk.get_bdf_stats())
 
+def test_get_parts_2d(model_filename):
+    """get_parts_2d test """
+    from bulk.bulk import Bulk
+
+    bulk = Bulk()
+    # get 2d parts from testing model
+    bulk.read_bulk(model_filename)
+    bulk.get_parts_2d()
 
 def main():
     """the interface for bulk_test"""
@@ -23,8 +31,10 @@ def main():
     bdf_filename = os.path.join(test_path, 'FEM-001.bdf')
 
     # read testing model
-    test_read_bulk(bdf_filename)
+    #test_read_bulk(bdf_filename)
 
+    # get 2d parts testing
+    test_get_parts_2d(bdf_filename)
 
 if __name__ == '__main__':
     main()
