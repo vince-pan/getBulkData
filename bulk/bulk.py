8c365a90e051ef6ee0d7639d94b4240bd949c162
matthias-hbt <matthias31770@gmail.com>
2022-11-15 23:00:00 +0100

    remove "self" in front of while loop called fonctions

--- bulk/bulk.py
+++ bulk/bulk.py
@@ -8,7 +8,7 @@ Main Bulk class.  Defines:
 """
 
 from pyNastran.bdf.bdf import BDF
-from bulk.bulk_utils import _set_selected_nids, _get_attached_eid_from_nid
+from bulk.bulk_utils import _set_selected_nids, _get_attached_eid_from_nid, _remove_selected_nids_from_bulk_nids
 
 class Bulk(BDF):
     """
@@ -49,22 +49,22 @@ class Bulk(BDF):
         Get 2D parts of a finite element model
         """
         # list of all node ids
-        self._bulk_nids = list(bulk.node_ids)
+        self._bulk_nids = list(self.node_ids)
 
         # blank part list
         self._part_list = {}
 
         # blank element ids list
-        self._attached_eids = {}
+        self._attached_eids = []
 
         # blank node list from which search for elements
-        self._selected_nids = {}
+        self._selected_nids = []
 
         # while bulk_nids is not empty, search for attached elements
-        #while self._bulk_nids.len() >= 0:
-            #self._selected_nids = _set_selected_nids(bulk)
-            #self._bulk_nids.remove(self._selected_nids)
-            #self._attached_eid, part_list = _get_attached_eid_from_nid(bulk)
+        while len(self._bulk_nids) >= 0:
+            _set_selected_nids(self)
+            _remove_selected_nids_from_bulk_nids(self)
+            _get_attached_eid_from_nid(self)
 
 class PART():
     """
