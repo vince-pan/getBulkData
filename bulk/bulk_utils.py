544a8f22fed13851a5881a252fec10302cc7f914
matthias-hbt <matthias31770@gmail.com>
2022-11-15 19:40:16 +0100

    add ":" after else condition

--- bulk/bulk_utils.py
+++ bulk/bulk_utils.py
@@ -5,7 +5,7 @@ def _set_selected_nids(bulk):
     #if selected_nids is empty
     if not bulk.selected_nids:
         bulk.selected_nids = bulk.bulk_nids[0]
-    else
+    else:
         bulk.selected_nids = [bulk.attached_eids[i].node_ids for i in bulk.attached_eids]
 
 def _get_attached_eid_from_nid(bulk):
@@ -17,7 +17,7 @@ def _get_attached_eid_from_nid(bulk):
     for i in temp:
         if temp[i] in bulk.attached_eids:
             continue
-        else
+        else:
             #put the element in attached_eids if not already in
             bulk.attached_eids.append(temp[i])
 
@@ -26,5 +26,5 @@ def _get_attached_eid_from_nid(bulk):
         cur_part = PART()
         cur_part.elements = bulk.attached_eids
         bulk.part_list.append(cur_part)
-    else
+    else:
         pass
