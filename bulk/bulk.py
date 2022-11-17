8a648bda0378211064202b8467bab65094e937fa
matthias-hbt <matthias31770@gmail.com>
2022-11-17 20:55:14 +0100

    fonction move to bulk.py

--- bulk/bulk.py
+++ bulk/bulk.py
@@ -8,7 +8,7 @@ Main Bulk class.  Defines:
 """
 
 from pyNastran.bdf.bdf import BDF
-from bulk.bulk_utils import _set_selected_nids, _get_attached_eid_from_nid, _remove_selected_nids_from_bulk_nids
+
 
 class Bulk(BDF):
     """
@@ -49,30 +49,64 @@ class Bulk(BDF):
         Get 2D parts of a finite element model
         """
         # list of all node ids
-        self._bulk_nids = list(self.node_ids)
-
-        # blank part list
-        self._part_list = {}
+        _bulk_nids = list(self.node_ids)
 
         # blank element ids list
-        self._attached_eids = []
+        _attached_eids = []
 
-        # blank node list from which search for elements
-        self._selected_nids = []
+        # selected nids to search for attached elements
+        _selected_nids = []
 
         # while bulk_nids is not empty, search for attached elements
-        while len(self._bulk_nids) >= 0:
-            _set_selected_nids(self)
-            _remove_selected_nids_from_bulk_nids(self)
-            _get_attached_eid_from_nid(self)
+        while _bulk_nids:
+            _selected_nids = _set_selected_nids()
+            _bulk_nids = _remove_selected_nids_from_bulk_nids(self)
+            _attached_nids = _get_attached_eid_from_nid(self)
 
-class PART():
+
+def _set_selected_nids(bulk_nids, selected_nids, attached_eids):
     """
-    PART object
+    Select node ids from which search for attached element ids
+    """
+    # if selected_nids is empty
+    if not selected_nids:
+        selected_nids.append(bulk_nids[0])
+    else:
+        [selected_nids.append(attached_eids[i].node_ids) for i in attached_eids]
+    return selected_nids
+
+
+def _get_attached_eid_from_nid(selected_nids, attached_eids):
+    """
+    Get attached element ids from a node ids list
+    """
+    temp = get_eids_from_nid(selected_nids)
+    for i in range(len(temp)):
+        if temp[i] in attached_eids:
+            # ignore
+            continue
+        else:
+            # put the element in attached_eids if not already in
+            attached_eids.append(temp[i])
+    return attached_eids
+
+
+def _remove_selected_nids_from_bulk_nids(bulk_nids, selected_nids):
+    """
+    remove nodes in _selected_nids from _bulk_ids
+    """
+    for i in range(len(selected_nids)):
+        bulk_nids.remove(selected_nids[i])
+    return bulk_nids
+
+
+class Part2D:
+    """
+    Part2d object
     """
     def __init__(self):
         """
-        Initialize the PART object
+        Initialize Part2d object
         """
         self.id = None
         self.elements = {}
