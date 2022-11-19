bbf1152d37d14448dfd187024e4d810b7b0aa5c5
Vincent Panis <v.panis@hotmail.fr>
2022-11-19 11:41:51 +0100

    Creation of unitary test files for Bulk class

---
+++ bulk/test/test_unit_bulk.py
@@ -0,0 +1,12 @@
+from unittest import TestCase
+
+
+class TestBulk(TestCase):
+    def test_read_bulk(self):
+        self.fail()
+
+    def test__get_part_2d(self):
+        self.fail()
+
+    def test__search_for_part_2d(self):
+        self.fail()
