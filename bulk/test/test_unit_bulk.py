108a4c48b4dd97c628955a909df913038d175b63
Vincent Panis <v.panis@hotmail.fr>
2022-11-24 17:29:52 +0100

    Unit test for elements id consistency

--- bulk/test/test_unit_bulk.py
+++ bulk/test/test_unit_bulk.py
@@ -20,3 +20,10 @@ class TestRead(TestBulk):
     def test_nb_of_elements_in_parts(self):
         elm_nb = {key: len(value.elements) for key, value in self.bulk.part_2d.items()}
         self.assertEqual(elm_nb, {11: 150, 12: 150, 13: 50, 14: 75, 15: 75, 16: 50})
+
+    def test_2d_parts_element_id(self):
+        # check_dict = {part_id: element_id}
+        check_dict = {11: 11, 12: 12, 13: 13, 14: 15, 15: 16, 16: 14}
+        for key, part_2d_obj in self.bulk.part_2d.items():
+            for k in part_2d_obj.elements.keys():
+                self.assertEqual(str(check_dict[key]), str(k)[:2])
