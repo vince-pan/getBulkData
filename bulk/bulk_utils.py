49a3557b75f3b45475441e63d239dadd2afcc545
Vincent Panis <v.panis@hotmail.fr>
2022-11-24 20:28:38 +0100

    Test bulk utils

--- bulk/bulk_utils.py
+++ bulk/bulk_utils.py
@@ -1,1 +1,33 @@
+# coding: utf-8
 
+import os
+from bulk.bulk import Bulk
+
+
+def extend_elements_from_nids(bulk, nids):
+    """
+
+    :param bulk: instance of class Bulk
+    :param nids: list of node ids
+    :return:
+    """
+    return {}
+
+
+def main():
+    """the interface for bulk_test"""
+    # set file name of testing model
+    test_path = os.path.join('..', '..', 'models')
+    bdf_filename = os.path.join(test_path, 'FEM-001.bdf')
+
+    # create instance of class Bulk
+    bulk = Bulk()
+    # read testing model
+    bulk.read_bulk(bdf_filename)
+    #
+    part = extend_elements_from_nids(bulk, [14001])
+    print(part)
+
+
+if __name__ == '__main__':
+    main()
--- bulk/test/test_bulk.py
+++ bulk/test/test_bulk.py
@@ -2,12 +2,11 @@
 """the interface for bulk_test"""
 
 import os
+from bulk.bulk import Bulk
 
 
 def test_read_bulk(model_filename):
     """read_bulk test """
-    from bulk.bulk import Bulk
-
     # create instance of class Bulk
     bulk = Bulk()
     # read testing model
